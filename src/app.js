19716e5300696fe80e19f7a6f775968c44b80c0a
Tanmay <tanmayhere33@gmail.com>
2024-11-29 21:14:52 +0530

    postman testing

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res) => {
-    res.send("Hello"); 
+
+
+app.use( "/test",(req,res) => {
+    res.send("Hello lawda"); 
 });
 
+// app.use("/", (req,res) => {
+//     res.send("Hello"); 
+// });
+
+app.get( "/user" ,(req,res) => {
+    res.send({
+        firstname: "Tanmay",
+        lastname: "Ail"
+    });
+    
+});
+
+app.post ( "/user" ,(req,res) => {
+    res.send("Data Successfully saved ");
+
+
+})
 app.listen(7777 , () => {
     console.log("successfully running 7777")
 });
